2aa83207d90c9d63e8792e239112442fd4e48a34
arfes0e2b3c <webyata@gmail.com>
2022-01-30 18:59:26 +0900

    completed

--- js/index.js
+++ js/index.js
@@ -1,13 +1,47 @@
-let x = [];
-
-readAnswer();
-setAnswer();
 createTag();
-function readAnswer() {
+function createTag() {
+  let form = document.getElementById('AnswerViewForm');
+  
+  let div = document.createElement('div');
+  div.setAttribute('id', 'div');
+  form.before(div);
+
+  let copy = document.createElement('button');
+  copy.setAttribute('id', 'copy');
+  copy.textContent = 'コピー';
+  div.appendChild(copy);
+  
+  let pasteText = document.createElement('textarea');
+  pasteText.setAttribute('id', 'paste-textarea');
+  pasteText.placeholder = '答えを入力してください'
+  div.appendChild(pasteText);
+  
+  let paste = document.createElement('button');
+  paste.setAttribute('id', 'paste');
+  paste.textContent = 'ペースト';
+  div.appendChild(paste);
+  
+  let copyText = document.createElement('textarea');
+  copyText.setAttribute('id', 'copy-textarea');
+  div.appendChild(copyText);
+  
+}
+
+function copy() {
+  let copyText = document.getElementById('copy-textarea');
+  copyText.value = readValues();
+  copyText.select();
+  document.execCommand("copy");
+}
+
+document.querySelector("#copy").addEventListener("click", copy);
+
+function readValues() {
   const inputs = document.getElementsByTagName("input");
   const values = getValue(inputs);
-  x = JSON.stringify(values);
+  return JSON.stringify(values);
 }
+
 function getValue(inputs) {
   let values =[];
   for (const input of inputs) {
@@ -15,37 +49,23 @@ function getValue(inputs) {
   }
   return values;
 }
-function setAnswer(){
+
+function paste() {
+  const arr = document.getElementById('paste-textarea');
+  setAnswer(arr.value);
+}
+
+document.querySelector("#paste").addEventListener("click", paste);
+
+function setAnswer(values){
+  values = values.slice(2,-2).split('","');
+  console.log(values);
   const inputs = document.getElementsByTagName("input");
-    if(x[1] === inputs[1].value) {
-    for (let i=0;i<x.length;i++) {
-      inputs[i].value = x[i];
+  if(values[1] === inputs[1].value) {
+    console.log(values);
+    for (let i=0;i<values.length;i++) {
+      inputs[i].value = values[i];
     }
   }
 }
-function createTag() {
-  let content = document.getElementById('content');
-  
-  let pasteText = document.createElement('textarea');
-  pasteText.setAttribute('id', 'textarea');
-  pasteText.value = x
-  content.after(pasteText);
 
-  let copyText = document.createElement('textarea');
-  copyText.setAttribute('id', 'copy-textarea');
-  copyText.value = x
-  content.after(copyText);
-  
-  let button = document.createElement('button');
-  button.setAttribute('id', 'copy');
-  button.textContent = 'コピー'
-  content.after(button);
-
-}
-function copy() {
-  let copyText = document.getElementById('copy-textarea');
-  copyText.select();
-  document.execCommand("copy");
-}
-
-document.querySelector("#copy").addEventListener("click", copy);
